516231543c33db7ceea19f4f960fdb73fc83ecf2
Leilani Reyes <leilani3@stanford.edu>
2017-10-22 02:07:42 -0700

    Merges app changes

--- concensus/screens/ParticipantView.js
+++ concensus/screens/ParticipantView.js
@@ -9,11 +9,7 @@ import {
 } from 'react-native';
 import PollComponent from '../components/PollComponent.js'
 import VoteComponent from '../components/VoteComponent.js'
-<<<<<<< HEAD
 import DiscussionSection from '../components/DiscussionSection.js'
-=======
-import DiscussionComponent from '../components/DiscussionComponent.js'
->>>>>>> 2298c22... Progress
 
 export default class ParticipantView extends React.Component {
 
@@ -21,11 +17,8 @@ export default class ParticipantView extends React.Component {
     /* Go ahead and delete ExpoConfigView and replace it with your
     * content, we just wanted to give you a quick view of your config */
 
-<<<<<<< HEAD
     const navigate = this.props.navigation.navigate
 
-=======
->>>>>>> 2298c22... Progress
     let pic = {
       uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
     };
@@ -54,17 +47,10 @@ export default class ParticipantView extends React.Component {
           title="Should we get bagels?"
           description="placeholder for discussion"></PollComponent>
           <Text>Hello world!</Text>
-<<<<<<< HEAD
           <VoteComponent navigate={navigate}></VoteComponent>
         </View>
         <View style={styles.view}>
           <DiscussionSection pollID={"HI"}></DiscussionSection>
-=======
-          <VoteComponent></VoteComponent>
-        </View>
-        <View style={styles.view}>
-          <DiscussionComponent threads={"Red hair crookshanks bludger Marauder’s Map Prongs sunshine daisies butter mellow Ludo Bagman. Beaters gobbledegook N.E.W.T., Honeydukes eriseD inferi Wormtail. Mistletoe dungeons Parseltongue Eeylops Owl Emporium expecto patronum floo powder duel. Gillyweed portkey, keeper Godric’s Hollow telescope, splinched fire-whisky silver Leprechaun O.W.L. stroke the spine. Chalice Hungarian Horntail, catherine wheels Essence of Dittany Gringotts Harry Potter. Prophecies Yaxley green eyes Remembrall horcrux hand of the servant. Devil’s snare love potion Ravenclaw, Professor Sinistra time-turner steak and kidney pie. Cabbage Daily Prophet letters from no one Dervish and Banges leg."}></DiscussionComponent>
->>>>>>> 2298c22... Progress
         </View> 
       </ScrollView>
 
