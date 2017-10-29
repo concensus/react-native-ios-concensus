ba5eb4e5c8dc013b44d492503f306634c1b40754
Ryan Cheung <cheung31@gmail.com>
2017-10-28 23:43:21 -0700

    Fix bugs in NewPollScreen where callbacks needed proper context, now provided by arrow functions. Sent a transaction to the testrpc.

--- screens/NewPollScreen.js
+++ screens/NewPollScreen.js
@@ -1,26 +1,38 @@
 import React from 'react';
 import { View, ScrollView } from 'react-native';
-import { Poll } from '../lib/concensus-sdk';
+import PollClient from '../lib/concensus-sdk/web3/poll';
 import ConcensusButton from '../components/ConcensusButton';
 const t = require('tcomb-form-native');
 const Form = t.form.Form;
 
-const NewPollScreen = ({ navigation }) => {
-    function onProposePress() {
-        navigation.navigate('QRCodeShower');
+const Poll = new PollClient();
+const VOTES_ALLOWED = 10;
 
-        Poll.create();
-    }
+class NewPollScreen extends React.Component {
+  onProposePress() {
+    const { navigation } = this.props;
+    navigation.navigate('QRCodeShower');
+    const formValues = this.refs.form.getValue();
+    Poll.create({
+      title: formValues.subject,
+      proposition: formValues.proposal,
+      endTime: formValues.endsInMinutes,
+      votesRequired: (formValues.consensusPercentage / 100) * VOTES_ALLOWED,
+      votesAllowed: VOTES_ALLOWED
+    });
+  }
 
+  render() {
     return (
-        <View style={{ padding: 20 }}>
-            <ScrollView>
-                <Form type={PollType} />
-            </ScrollView>
-            <ConcensusButton label="Propose Motion" onPress={onProposePress} />
-        </View>
+      <View style={{padding: 20}}>
+        <ScrollView>
+          <Form ref="form" type={PollType}/>
+        </ScrollView>
+        <ConcensusButton label="Propose Motion" onPress={this.onProposePress.bind(this)}/>
+      </View>
     );
-};
+  }
+}
 
 NewPollScreen.navigationOptions = ({ navigation }) => ({
     title: 'Propose a Motion',
